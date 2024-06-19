f2fa9d9c406440a2aeccf3c4183b90f42c869c07
KnockOutEZ <ktowhid20@gmail.com>
2024-06-19 17:29:07 +0600

    updated go example for multi node cluster

--- examples/go/main.go
+++ examples/go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/pgEdge/pulumi-pgedge/sdk/go/pgedge"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -9,10 +11,12 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 	cluster, err := pgedge.NewCluster(ctx, "cluster", &pgedge.ClusterArgs{
-		Name:           pulumi.String("n1"),
-		CloudAccountId: pulumi.String(""), // cloud account id
+		Name:           pulumi.String("testcluster"),
+		CloudAccountId: pulumi.String(os.Getenv("PGEDGE_CLOUD_ACCOUNT_ID")),
 		Regions: pulumi.StringArray{
+			pulumi.String("us-east-1"),
 			pulumi.String("us-east-2"),
+			pulumi.String("eu-central-1"),
 		},
 		FirewallRules: pgedge.ClusterFirewallRuleArray{
 			&pgedge.ClusterFirewallRuleArgs{
@@ -27,17 +31,48 @@ func main() {
 				InstanceType:     pulumi.String("t4g.small"),
 				Name:             pulumi.String("n1"),
 				VolumeSize:       pulumi.Int(30),
+				Region:           pulumi.String("us-east-1"),
+				AvailabilityZone: pulumi.String("us-east-1a"),
+				VolumeType:       pulumi.String("gp2"),
+			},
+			&pgedge.ClusterNodeArgs{
+				InstanceType:     pulumi.String("t4g.small"),
+				Name:             pulumi.String("n2"),
+				VolumeSize:       pulumi.Int(30),
 				Region:           pulumi.String("us-east-2"),
 				AvailabilityZone: pulumi.String("us-east-2a"),
 				VolumeType:       pulumi.String("gp2"),
 			},
+			&pgedge.ClusterNodeArgs{
+				InstanceType:     pulumi.String("t4g.small"),
+				Name:             pulumi.String("n2"),
+				VolumeSize:       pulumi.Int(30),
+				Region:           pulumi.String("eu-central-1"),
+				AvailabilityZone: pulumi.String("eu-central-1a"),
+				VolumeType:       pulumi.String("gp2"),
+			},
 		},
+
 		Networks: pgedge.ClusterNetworkArray{
+			&pgedge.ClusterNetworkArgs{
+				Region: pulumi.String("us-east-1"),
+				Cidr:   pulumi.String("10.1.0.0/16"),
+				PublicSubnets: pulumi.ToStringArray([]string{
+					"10.1.1.0/24",
+				}),
+			},
 			&pgedge.ClusterNetworkArgs{
 				Region: pulumi.String("us-east-2"),
 				Cidr:   pulumi.String("10.2.0.0/16"),
 				PublicSubnets: pulumi.ToStringArray([]string{
 					"10.2.1.0/24",
+				}),
+			},
+			&pgedge.ClusterNetworkArgs{
+				Region: pulumi.String("eu-central-1"),
+				Cidr:   pulumi.String("10.3.0.0/16"),
+				PublicSubnets: pulumi.ToStringArray([]string{
+					"10.3.1.0/24",
 				}),
 			},
 		},
@@ -49,8 +84,8 @@ func main() {
 
 	_, err = pgedge.NewDatabase(ctx, "database", &pgedge.DatabaseArgs{
 		ClusterId: cluster.ID(),
-		Name:      pulumi.String("defaultdb"),
-		// Options:   pulumi.StringArray{pulumi.String("replication = '1'")},
+		Name:      pulumi.String("testdb"),
+		Options:   pulumi.StringArray{pulumi.String("install:northwind"), pulumi.String("autoddl:enabled")},
 	})
 
 	if err != nil {
